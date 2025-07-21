921f8dcff344b8deab66fde4065610c9ab563a64
Evtim Kirov <evtimkirov@gmail.com>
2025-07-21 16:27:00 +0300

    Product price and promotion API endpoint

--- market/app/Http/Controllers/API/ProductController.php
+++ market/app/Http/Controllers/API/ProductController.php
@@ -4,11 +4,63 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Services\BasePriceCalculatorService;
+use App\Services\BundleDiscountDecorator;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    /**
+     * Handle API endpoint for calculation between the product price and the available promotion
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function calculate(Request $request)
     {
-        return response()->json(['products' => Product::all()]);
+        $productId = $request->input('product_id');
+        $quantity = $request->input('quantity');
+
+        return response()
+            ->json(
+                ['total_price' => $this->getProductTotalPriceWithPromotion($productId, $quantity)]
+            );
+    }
+
+    /**
+     * Gets the product total price calculated with the current promotion
+     *
+     * @param $productId
+     * @param $quantity
+     * @return int
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    private function getProductTotalPriceWithPromotion($productId, $quantity)
+    {
+        $product = Product::findOrFail($productId);
+        $promotion = $product->promotion()->first();
+
+        if ($promotion) {
+            $productBundlePriceService = app()->make(
+                BundleDiscountDecorator::class,
+                [
+                    'calculator' => new BasePriceCalculatorService($product->price),
+                    'bundlePrice' => $promotion->total,
+                    'bundleQuantity' => $promotion->quantity,
+                ]
+            );
+
+            $totalPrice = $productBundlePriceService->calculate($quantity);
+        } else {
+            $dd = app()
+                ->make(
+                    BasePriceCalculatorService::class,
+                    ['unitPrice' => $product->price]
+                );
+            $totalPrice = $dd->calculate($quantity);
+        }
+
+        return $totalPrice;
     }
 }
--- market/routes/api.php
+++ market/routes/api.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\API\ProductController;
 use App\Http\Middleware\CheckToken;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(CheckToken::class)->group(function () {
-    Route::get(
-        '/products',
-        [
-            ProductController::class,
-            'index',
-        ]
-    );
+Route::middleware(CheckToken::class)
+    ->group(function () {
+        Route::post(
+            'products/calculate',
+            [
+                ProductController::class,
+                'calculate',
+            ]
+        );
 });
