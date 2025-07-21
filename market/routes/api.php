491dce0d8af34884a36523c1711e134ac44bfe3d
Evtim Kirov <evtimkirov@gmail.com>
2025-07-21 11:04:44 +0300

    Middleware with the API auth

--- market/routes/api.php
+++ market/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckToken;
+use App\Http\Controllers\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware(CheckToken::class)->group(function () {
+    Route::get(
+        '/products',
+        [
+            ProductController::class,
+            'index',
+        ]
+    );
+});
