eb1b5059e6e59b4c60bedf76e1ad6b4708a0f099
Evtim Kirov <evtimkirov@gmail.com>
2025-07-23 12:44:36 +0300

    Admin routes

--- market/routes/admin.php
+++ market/routes/admin.php
@@ -1,3 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\OrderController;
+
+Route::middleware('auth')
+    ->group(function () {
+        // Product routes
+        Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+        Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+        Route::get('/products/{id}', [ProductController::class, 'edit'])->name('products.edit');
+        Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+        Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+        Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.delete');
+
+        // Order routes
+        Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
+        Route::put('/admin/orders/{order}/status', [OrderController::class, 'updateStatus'])
+            ->name('orders.updateStatus');
+    });
--- market/routes/web.php
+++ market/routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', [ProductController::class, 'index']);
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 // Admin routes
 Route::prefix('admin')
